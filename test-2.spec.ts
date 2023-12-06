da17ffdd287cd040bb634dcd7a9f439ef2da9f65
krokedilmartin <84283336+krokedilmartin@users.noreply.github.com>
2023-12-06 08:32:35 +0100

    WC 8.4 compatible

--- test-2.spec.ts
+++ test-2.spec.ts
@@ -1,71 +1,73 @@
 import { test } from '@playwright/test';
 
-let URL = 'https://okt-test-resurs.krokedil.site/'
-let userName = 'replace'
-let passWord = 'replace'
+let URL = 'https://kco-test5dec.krokedil.site/'
+let userName = 'automaten'
+let passWord = 'PQ6m33e%cCOViZ$G@p3XWEve'
 
-test('cart1', async ({ page }) => {
+test('cart11', async ({ page }) => {
 
-  await page.goto(URL + 'wp-login.php?redirect_to=https%3A%2F%2Fautomat-martin.krokedil.site%2Fwp-admin%2F&reauth=1');
+  await page.goto(URL + 'wp-login.php?redirect_to=https%3A%2F%2Fkco-test5dec.krokedil.site%2Fwp-admin%2F&reauth=1');
+  await page.getByLabel('Username or Email Address').click();
   await page.getByLabel('Username or Email Address').fill(userName);
   await page.getByLabel('Password', { exact: true }).click();
   await page.getByLabel('Password', { exact: true }).fill(passWord);
   await page.getByRole('button', { name: 'Log In' }).click();
-  await page.goto(URL);
 
   await page.goto(URL);
-  await page.getByLabel('Add “Downloadable 25%” to your basket').click();
+
+  await page.getByLabel('Add to cart:  “Downloadable 25%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Virtual 25%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Virtual 25%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 25%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 25%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);
 
-  await page.getByLabel('Add “Simple 12%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Simple 12%” ').click();
   await page.goto(URL);  
 
-  await page.goto(URL + 'product/variable-25/?attribute_color=Red');
+  await page.getByRole('link', { name: 'Placeholder Variable 25% 100,00 kr' }).click();
+  await page.getByRole('row', { name: 'Color', exact: true }).locator('th').click();
+  await page.getByLabel('Color').selectOption('Red');
   await page.waitForTimeout(1000);
-
   await page.getByRole('button', { name: 'Add to basket' }).click();
   await page.goto(URL + 'checkout/');
 
-
 });
 
-test('cart2', async ({ page }) => {
+test('cart22', async ({ page }) => {
 
-  await page.goto(URL + 'wp-login.php?redirect_to=https%3A%2F%2Fautomat-martin.krokedil.site%2Fwp-admin%2F&reauth=1');
+  await page.goto(URL + 'wp-login.php?redirect_to=https%3A%2F%2Fkco-test5dec.krokedil.site%2Fwp-admin%2F&reauth=1');
+  await page.getByLabel('Username or Email Address').click();
   await page.getByLabel('Username or Email Address').fill(userName);
   await page.getByLabel('Password', { exact: true }).click();
   await page.getByLabel('Password', { exact: true }).fill(passWord);
   await page.getByRole('button', { name: 'Log In' }).click();
-  await page.goto(URL);
+
 
   await page.goto(URL);
-  await page.getByLabel('Add “Downloadable Virtual 0%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Downloadable Virtual 0%” ').click();
   await page.goto(URL);
-  await page.getByLabel('Add “Downloadable Virtual 25%” to your basket').click();
+  await page.getByLabel('Add to cart:  “Downloadable Virtual 25%” ').click();
 
 
   await page.goto(URL + 'checkout/');
